ca60f44810644b9e68e1c14665be22300830c090
DT <1466053692@qq.com>
2020-02-27 20:40:21 +0800

    dtt

---
+++ sjkcxx/src/test/java/com/sjkcxx/SjkcxxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjkcxx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjkcxxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
