f1536b51b67b6a1cc6329347b1071ab9bf82a367
duantong <1466053692@qq.com>
2020-04-26 17:22:01 +0800

    dtt

--- sjkcxx/src/main/java/com/sjkcxx/service/PracticeSubjectService.java
+++ sjkcxx/src/main/java/com/sjkcxx/service/PracticeSubjectService.java
@@ -13,14 +13,12 @@ import java.util.List;
  * @Description:实践课程service
  */
 public interface PracticeSubjectService extends IService<PracticeSubject> {
-
     /**
      * 审核课程
      * @param practiceSubject：学科信息
      * @return：成功标识符 0：失败，1：成功
      */
     int adminCheckSubject(PracticeSubject practiceSubject);
-
     /**
      * 学生选课
      * @param practiceSubject：学科信息
@@ -28,7 +26,6 @@ public interface PracticeSubjectService extends IService<PracticeSubject> {
      * @return：成功标识符 0：失败，1：成功
      */
     int studentSelectSubject(PracticeSubject practiceSubject, StudentSubject studentSubject);
-
     /**
      * 学生多条件查询已审核学科信息
      * @param practiceSubject：学科信息
@@ -36,7 +33,6 @@ public interface PracticeSubjectService extends IService<PracticeSubject> {
      * @return：分页封装数据信息
      */
     PageDto<PracticeSubject> studentQuerySubjectLike(PracticeSubject practiceSubject, PageDto pageDto);
-
     /**
      * 多条件查询学科信息
      * @param practiceSubject：学科信息
--- sjkcxx/src/main/java/com/sjkcxx/service/StudentSubjectService.java
+++ sjkcxx/src/main/java/com/sjkcxx/service/StudentSubjectService.java
@@ -9,32 +9,27 @@ import com.sjkcxx.mapper.StudentSubjectMapper;
 /**
  * @Author: duant
  * @Date: 2020/3/7 15:14
- * @Description:
+ * @Description:学生成绩相关业务
  */
 public interface StudentSubjectService extends IService<StudentSubject> {
-
     /**
      * 上传作业
      * @param studentSubject：学生成绩信息
      * @return：成功标识符 0：失败，1：成功
      */
     int studentUpload(StudentSubject studentSubject);
-
     /**
      * 教师打分
      * @param studentSubject：学生成绩信息
      * @return：成功标识符 0：失败，1：成功
      */
     int gradeToStudent(StudentSubject studentSubject);
-
     /**
      * 学生打分
      * @param studentSubject：学生成绩信息
      * @return：成功标识符 0：失败，1：成功
      */
     int studentScoreRate(StudentSubject studentSubject);
-
-
     /**
      * 学生课程列表展示
      * @param studentSubject：
