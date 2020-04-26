322f57d41bb78ea9a99102e08d3989de97a24620
duantong <1466053692@qq.com>
2020-04-26 17:08:02 +0800

    dtt

--- sjkcxx/src/main/java/com/sjkcxx/service/PracticeSubjectService.java
+++ sjkcxx/src/main/java/com/sjkcxx/service/PracticeSubjectService.java
@@ -16,30 +16,37 @@ public interface PracticeSubjectService extends IService<PracticeSubject> {
 
     /**
      * 审核课程
-     * @param practiceSubject
-     * @return
+     * @param practiceSubject：学科信息
+     * @return：成功标识符 0：失败，1：成功
      */
     int adminCheckSubject(PracticeSubject practiceSubject);
 
     /**
      * 学生选课
-     * @param practiceSubject
-     * @param studentSubject
-     * @return
+     * @param practiceSubject：学科信息
+     * @param studentSubject：学生成绩信息
+     * @return：成功标识符 0：失败，1：成功
      */
     int studentSelectSubject(PracticeSubject practiceSubject, StudentSubject studentSubject);
 
     /**
      * 学生多条件查询已审核学科信息
-     * @param practiceSubject
-     * @return
+     * @param practiceSubject：学科信息
+     * @param pageDto：分页信息数据封装信息（需要展示的页码等信息）
+     * @return：分页封装数据信息
      */
     PageDto<PracticeSubject> studentQuerySubjectLike(PracticeSubject practiceSubject, PageDto pageDto);
 
     /**
      * 多条件查询学科信息
-     * @param practiceSubject
-     * @return
+     * @param practiceSubject：学科信息
+     * @return：分页封装数据信息
+     */
+    /**
+     *
+     * @param practiceSubject：学科信息
+     * @param pageDto：分页信息数据封装信息（需要展示的页码等信息）
+     * @return：分页封装数据信息
      */
     PageDto<PracticeSubject> querySubjectLike(PracticeSubject practiceSubject, PageDto pageDto);
 
--- sjkcxx/src/main/java/com/sjkcxx/service/StudentSubjectService.java
+++ sjkcxx/src/main/java/com/sjkcxx/service/StudentSubjectService.java
@@ -15,31 +15,31 @@ public interface StudentSubjectService extends IService<StudentSubject> {
 
     /**
      * 上传作业
-     * @param studentSubject
-     * @return
+     * @param studentSubject：学生成绩信息
+     * @return：成功标识符 0：失败，1：成功
      */
     int studentUpload(StudentSubject studentSubject);
 
     /**
      * 教师打分
-     * @param studentSubject
-     * @return
+     * @param studentSubject：学生成绩信息
+     * @return：成功标识符 0：失败，1：成功
      */
     int gradeToStudent(StudentSubject studentSubject);
 
     /**
      * 学生打分
-     * @param studentSubject
-     * @return
+     * @param studentSubject：学生成绩信息
+     * @return：成功标识符 0：失败，1：成功
      */
     int studentScoreRate(StudentSubject studentSubject);
 
 
     /**
      * 学生课程列表展示
-     * @param studentSubject
-     * @param pageDto
-     * @return
+     * @param studentSubject：
+     * @param pageDto：分页信息数据封装信息（需要展示的页码等信息）
+     * @return：分页学生成绩信息数据封装信息
      */
     PageDto<StudentSubject> queryStudentSubjectLike(StudentSubject studentSubject, PageDto pageDto);
 
